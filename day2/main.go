df1171000cfc2c61a584dbf38dc016aac4dc65f6
Richie Jang <richiejang@naver.com>
2024-12-04 07:49:54 +0900

    Solved day2 by GO

--- day2/main.go
+++ day2/main.go
@@ -15,12 +15,14 @@ const (
 	input = "input.txt"
 )
 
-func checkLine(s string) bool {
+func strToIntSlice(s string) []int {
 	ss := strings.Split(s, " ")
-	ar := lo.Map(ss, func(item string, _ int) int {
+	return lo.Map(ss, func(item string, _ int) int {
 		return lo.Must1(strconv.Atoi(item))
 	})
+}
 
+func checkLine(ar []int) bool {
 	checking := func(i int) (isOk bool, dir int) {
 		isOk = true
 		v := ar[i] - ar[i+1]
@@ -53,23 +55,66 @@ func checkLine(s string) bool {
 	return true
 }
 
-func main() {
-	f, er := os.Open(input)
-	if er != nil {
-		log.Fatal(er)
-	}
-	defer f.Close()
-	br := bufio.NewScanner(f)
+func part1(br *bufio.Scanner) {
 	res := 0
 	for br.Scan() {
 		t := br.Text()
 		if t == "" {
 			continue
 		}
-		if checkLine(t) {
+		ints := strToIntSlice(t)
+		if checkLine(ints) {
 			res++
 		}
 	}
-
 	fmt.Println(res)
+}
+
+func resolveData(ints []int) bool {
+	for i := 0; i < len(ints); i++ {
+		ndata := make([]int, 0)
+		if i == 0 {
+			ndata = append(ndata, ints[1:]...)
+		} else {
+			ndata = append(ndata, ints[:i]...)
+			ndata = append(ndata, ints[i+1:]...)
+		}
+		//fmt.Println("r:", i, " ", ints, " ", ndata)
+		if checkLine(ndata) {
+			return true
+		}
+	}
+	return false
+}
+
+func part2(br *bufio.Scanner) {
+	res := 0
+	for br.Scan() {
+		t := br.Text()
+		if t == "" {
+			continue
+		}
+		ints := strToIntSlice(t)
+		if checkLine(ints) {
+			res++
+		} else {
+			if resolveData(ints) {
+				res++
+			}
+		}
+	}
+	fmt.Println(res)
+}
+
+func main() {
+	f, er := os.Open(input)
+	//f, er := os.Open("sample.txt")
+	if er != nil {
+		log.Fatal(er)
+	}
+	defer f.Close()
+	br := bufio.NewScanner(f)
+
+	//part1(br)
+	part2(br)
 }
